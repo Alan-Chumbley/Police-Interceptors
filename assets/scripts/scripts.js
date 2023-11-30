ce039c2b3fa717db302d0df62f4b43331b95d2fd
jaycossey <jaycossey@live.com>
2023-11-30 21:38:08 +0000

    added functionality for search input and button while keeping mapbox functionality, created global array to store the created maps, added comments to JS, need to locally store data for back reference (DO NOT DO THIS WITH POLICE DATA, far too much of it) - ideally latLon and map

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,4 +1,4 @@
-// GLOBALS -----------------------------------------------------------
+// GLOBALS ---------------------------------------------------------------------------------------------------
 
 // API KEY VARIABLES
 const GEO_KEY = "8e65421b6e97a45d703a871ea4e78c3a";
@@ -9,6 +9,7 @@ let latLon;
 
 // NO LONGER GOOGLE, USING MAPBOX - Alans key, be careful
 const MAPBOX_KEY = "pk.eyJ1IjoiY2h1bWJhIiwiYSI6ImNscGw5a2k1NjAxemwybG83ZmE0ZGplYmYifQ.1MecnWfHuhj0e8vo1cYCkw";
+let map = [];
 
 // Police API no longer needs key
 const POLICE_KEY = "";
@@ -19,7 +20,7 @@ const POLICE_KEY = "";
 let monthSearched = "2021-01";
 let crimeData;
 
-// API CALLS AND DATA HANDLING
+// API CALLS AND DATA HANDLING ------------------------------------------------------------------------------
 
 // handle returned data from calls
 function handleData(data, apiName) {
@@ -33,9 +34,6 @@ function handleData(data, apiName) {
             latLon = {latitude: data.lat, longitude: data.lon};
             console.log("latLon = " + latLon.longitude + " " + latLon.longitude);
             return;
-        // google maps handling - current blocker
-        case "googleMap":
-            return "google data";
         // police data handling
         case "policeApi":
             // update global with data as current returned array
@@ -65,11 +63,6 @@ async function callAPI(apiName, apiKey) {
                 // search city name -- BUG FOUND -- LOCATION ON POST CODE SEARCHED BUT NOT WITH CITY NAMES!! -- unsure why but will work on a fix
                 path = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + ",GB&appid=" + apiKey;   
             }
-            break;
-        // Google map pathing
-        case "googleMap":
-            path = "";
-            console.log(path);
             break;
         case "policeApi":
             // This link functions correctly and we have the data as a huge (835 total for just NN1) array of objects, need to figure out how to filter through it all
@@ -105,27 +98,68 @@ async function callAPI(apiName, apiKey) {
         .catch(error => console.log(apiName + " " + error));
 }
 
-// call api for geocoded location, assign lat / lon object on return value
-// callAPI("geoCode", GEO_KEY);
-
+// Delayed call for data to ensure lat lon
 function apiDelayedCall() {
     setTimeout(() => {
         callAPI("policeApi", POLICE_KEY);
+        setupMap([latLon.longitude, latLon.latitude]);
     }, 3000);
 }
 
+// MAPBOX CALLS AND DATA HANDLING ---------------------------------------------------------------------------
 
-//serach button ======
+// map key - ALANS API- please don't run an infinite loop $$$ - no worries wont do that, Ian :P 
+// AccessToken syntax for API key within MapBox
+mapboxgl.accessToken = MAPBOX_KEY;
+
+// fetch users current location
+navigator.geolocation.getCurrentPosition(success, error, {
+    enableHighAccuracy: true
+});
+// handle sucess call of geolocation
+function success(position) {
+    // function call for mapbox, current location
+    setupMap([position.coords.longitude, position.coords.latitude])//
+}
+// if user disables access to location, provide a default
+function error() {
+    // default location
+    setupMap([-0.083094, 51.511177]);
+}
+
+
+// function to create map on latlon 
+function setupMap(lonlat) {
+    // push new map to array (handle local storage for this)
+    map.push(new mapboxgl.Map({
+        // mapbox settings
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: lonlat, //centre location on page
+        zoom: 14
+    }));
+    // console.log(map);
+    // console.log(map[0]);
+}
+
+
+// SEARCH BUTTON FUNCTIONS ----------------------------------------------------------------------------------
+
+// handle search button click
 $("#search-button").on("click", function(event){
-    // event.preventDefault();
-    console.log("Button Clicked!");
+    // console.log("Button Clicked!");
+    // link up location to search with global
     city = $('#search-input').val();
+    // call geocode
     callAPI("geoCode", GEO_KEY);
+    // call delayed police data to ensure latLon update
     apiDelayedCall();
 });
 
+// handle search with return key same as search button
 $("#search-input").on('keypress', function(event) {
     if (event.key == 'Enter') {
+        // prevent form refresh default
         event.preventDefault();
         city = event.target.value;
         callAPI("geoCode", GEO_KEY);
@@ -133,38 +167,3 @@ $("#search-input").on('keypress', function(event) {
     }
     
 });
-
-
-//   map key - ALANS API- please don't run an infinite loop $$$ - no worries wont do that :P 
-mapboxgl.accessToken = MAPBOX_KEY;
-  
-
-   //current location
-   navigator.geolocation.getCurrentPosition(success, error, {
-    enableHighAccuracy: true
-  })
-  
-  function success(position) {
-    console.log("Current Location: " && position); //resurns users location
-    setupMap([position.coords.longitude, position.coords.latitude])//
-    
-
-  }
-  //if user disables access to location, provide a default
-  function error() {
-    setupMap([-0.083094, 51.511177])
-  }
-
-  //map settings centered to be more intuitive
-  function setupMap(center) {
-    var map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: center, //centre location on page
-      zoom: 14 
-      
-    })
-   
-  }
-//   localStorage.setItem
-
